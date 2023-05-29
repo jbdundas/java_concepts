49b3e58baa2e7e861c53841a90b06a1e6db1b1af
jbdundas <jbdundas@fedora>
2023-05-29 15:31:31 -0400

    Add streams example.

--- src/main/java/com/jd/main/Main.java
+++ src/main/java/com/jd/main/Main.java
@@ -1,14 +1,30 @@
 package com.jd.main;
 
 import com.jd.leetcode.IsSubSequence;
+import com.jd.prep.java.StreamsExample;
 
 public class Main {
 
 	public static void main(String[] args) {
+		generateSubSequence();
+		
+		generateFlatMap(); 
+		
+	}
+
+	private static void generateFlatMap() {
+		//Examples about Java Streams.
+		StreamsExample streams1 = new StreamsExample();
+		streams1.testFlatMap();
+	}
+
+	private static void generateSubSequence() {
 		IsSubSequence isSubSequence = new IsSubSequence();
-		isSubSequence.isSubsequence("abc","ahbgdc");//CORRECT
-		isSubSequence.isSubsequence( "axc","ahbgdc");//CORRECT
-		isSubSequence.isSubsequence("acb","ahbgdc"); //WRONG ANSWERR. It should be false.
+		isSubSequence.isSubsequence("abc","ahbgdc");
+		isSubSequence.isSubsequence( "axc","ahbgdc");
+		isSubSequence.isSubsequence("acb","ahbgdc"); 
 		isSubSequence.isSubsequence("ab", "baab");
 	}
+	
+	
 }
--- src/main/java/com/jd/prep/java/StreamsExample.java
+++ src/main/java/com/jd/prep/java/StreamsExample.java
@@ -1,6 +1,5 @@
 package com.jd.prep.java;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -9,10 +8,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public class Streams1 {
+public class StreamsExample {
 
 	public static final Logger log = Logger.getLogger("Streams1");
 	
+	/*
+	 * @author jitesh dundas
+	 * When given a map, use the flatMap streams method to 
+	 */
 	public void testFlatMap() {
 		
 		Map<String, List<String>> map1 = new HashMap<>();
@@ -20,7 +23,8 @@ public class Streams1 {
 		map1.put("Marketing",Arrays.asList(new String[]{"EFG","HIJ"}));
 		
 		var list1 = map1.values().stream().flatMap( x-> x.stream()).collect(Collectors.toList());
-	
+		
 		log.log(Level.INFO, "Result--{0}",list1.toString() );
 	}
 }
+
