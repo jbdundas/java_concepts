558474b9f15e36951ca8e78e79e599125f0786e6
Jitesh Dundas <jbdundas@gmail.com>
2023-06-08 23:18:39 -0400

    First Occurrence of non-repeating character in the string

--- src/main/java/com/jd/main/Main.java
+++ src/main/java/com/jd/main/Main.java
@@ -1,22 +1,41 @@
 package com.jd.main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.jd.java.collectors.GroupingByExample;
 
 import com.jd.leetcode.IsSubSequence;
+import com.jd.prep.java.CharacterOccurence;
 import com.jd.prep.java.StreamsExample;
 
 public class Main {
 
+	public static final Logger log = Logger.getLogger("Main");
+	
 	public static void main(String[] args) {
+		printLineSeparator();
 		generateSubSequence();
 		
+		printLineSeparator();
 		generateFlatMap(); 
 		
+		printLineSeparator();	
 		GroupingByExample groupingByExample = new GroupingByExample();
 		groupingByExample.printEmployeesCountByDept(100);
 		
+		printLineSeparator();
 		groupingByExample.printEmployeesByDept(100);
 		
+		printLineSeparator();
+		CharacterOccurence characterOccurence = new CharacterOccurence();
+		characterOccurence.stringOccurence("hello");
+		
+		printLineSeparator();
+	}
+
+	private static void printLineSeparator() {
+		log.log(Level.INFO, "--------------------------------------------");
 	}
 
 	private static void generateFlatMap() {
--- src/main/java/com/jd/prep/java/CharacterOccurence.java
+++ src/main/java/com/jd/prep/java/CharacterOccurence.java
@@ -1,18 +1,45 @@
 package com.jd.prep.java;
 
-import java.util.Arrays;
-import java.util.function.BiFunction;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /*
- * Find the number of occurrences of each character in THE GIVEN string using streams.
+ * Find the First Occurrence of non-repeating character in the input string.
  */
 public class CharacterOccurence {
 
+	public static final Logger log = Logger.getLogger("CharacterOccurence");
 	
 	public void stringOccurence(String input) {
 		
+		var tempList = input.chars()
+				.mapToObj(x -> (char)x)
+				.collect(Collectors.toList());
+						
+		var result = tempList.stream()
+				.map(x -> {
+							var count = Collections.frequency(tempList, (char) x);
+							return new CharRecord( (char) x,count);
+				})
+				.filter(x -> (x.count <= 1) ).findFirst();
 		
+		log.log(Level.INFO, "First Occurrence of non-repeating character in the string-{0} is \n {1}",new Object[] {input,result.get().beautify()});
 		
+	}
+}
+
+class CharRecord{
+	char ch;
+	int count;
+	public CharRecord(char ch, int count) {
+		super();
+		this.ch = ch;
+		this.count = count;
+	}
+	
+	public String beautify() {
+		return "{value:"+ch + "," + "count:" + count + "}";
 	}
 }
