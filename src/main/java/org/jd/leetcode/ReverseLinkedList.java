09626537c70efceca7a9433b610d63916d21858a
Jitesh Dundas <jbdundas@gmail.com>
2023-07-25 11:42:32 -0400

    Added skeleton for reverse linked list problem.

---
+++ src/main/java/org/jd/leetcode/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.jd.leetcode;
+
+/**
+ * @author jbdundas
+ * @date 25 July 2023
+ * 206. Reverse Linked List
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * Example 1:
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ReverseLinkedList {
+	
+	    public ListNode reverseList(ListNode head) {
+			return head;
+	        
+	    }
+	
+}
